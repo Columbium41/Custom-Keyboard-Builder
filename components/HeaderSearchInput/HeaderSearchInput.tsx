c25ebac74790b9ddb95809d79f38f0689959fda4
Charley Liu <charleyliu716@gmail.com>
2024-05-20 18:46:36 -0400

    small changes

--- components/HeaderSearchInput/HeaderSearchInput.tsx
+++ components/HeaderSearchInput/HeaderSearchInput.tsx
@@ -15,7 +15,7 @@ export default function HeaderSearchInput() {
         <div id="search-text-input-container" className="w-3/5 h-3/4 my-auto relative">
             <input 
             type="text"
-            placeholder="Search"
+            placeholder="Search for parts"
             value={searchQuery}
             onChange={handleChange}
             className="w-full h-full bg-transparent text-neutral-200 pl-1 focus:outline-none" 
