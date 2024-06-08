8ffd84d70a1f1d7d0e84f2d35aad8291b8619727
Charley Liu <charleyliu716@gmail.com>
2024-06-08 14:49:37 -0400

    re-add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,14 @@ import "@/app/globals.css";
 import {Toaster} from "react-hot-toast";
 import {ToastProvider} from "@/app/providers/ToastProviders";
 
+import type {Metadata} from "next";
+
 const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+    title: "Custom Keyboard Builder",
+    description: "A web application that allows users to build their own custom keyboards and share them with other people",
+};
 
 export default function RootLayout({
     children,
