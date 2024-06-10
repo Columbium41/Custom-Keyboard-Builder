f7700194f6da18aec43bf60b31fad713a86082d3
Charley Liu <charleyliu716@gmail.com>
2024-06-10 11:39:48 -0400

    add sign in check for auth pages

--- app/(auth)/signin/page.tsx
+++ app/(auth)/signin/page.tsx
@@ -9,15 +9,16 @@ export default function SignInPage() {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const router = useRouter();
-    const { data: session } = useSession();
-    const { showToastPromise } = useToastContext();
+    const { data: session, status } = useSession();
+    const { showToast, showToastPromise } = useToastContext();
 
     // check if user is already signed in
     useEffect(() => {
-        if (session) {
+        if (status === 'authenticated') {
+            showToast('You are already signed in', {}, 'error');
             router.push('/');
         }
-    }, [session, router]);
+    }, [status]);
 
     const handleSignIn = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -45,40 +46,44 @@ export default function SignInPage() {
         });
     };
 
-    return (
-        <div className="w-96 h-full mx-auto flex flex-col align-middle">
-            <div className="my-auto px-4 py-5 bg-neutral-800 text-white rounded-3xl">
-                <h1 className="text-center font-bold text-2xl mb-3">Sign into your Account</h1>
-                <form onSubmit={handleSignIn} className="flex flex-col justify-center">
-                    <div className="mb-3">
-                        <label htmlFor="email-input" className="ml-0.5">Email:</label>
-                        <input
-                            id="email-input"
-                            type="email"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={email}
-                            placeholder="hello@example.com"
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password-input" className="ml-0.5">Password:</label>
-                        <input
-                            id="password-input"
-                            type="password"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
+    if (status === 'loading' || status === 'authenticated') {
+        return <></>
+    } else {
+        return (
+            <div className="w-96 h-full mx-auto flex flex-col align-middle">
+                <div className="my-auto px-4 py-5 bg-neutral-800 text-white rounded-3xl">
+                    <h1 className="text-center font-bold text-2xl mb-3">Sign into your Account</h1>
+                    <form onSubmit={handleSignIn} className="flex flex-col justify-center">
+                        <div className="mb-3">
+                            <label htmlFor="email-input" className="ml-0.5">Email:</label>
+                            <input
+                                id="email-input"
+                                type="email"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={email}
+                                placeholder="hello@example.com"
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password-input" className="ml-0.5">Password:</label>
+                            <input
+                                id="password-input"
+                                type="password"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <button type="submit" className="bg-blue-700 px-4 py-2 rounded-md mx-auto">
-                        Login
-                    </button>
-                </form>
+                        <button type="submit" className="bg-blue-700 px-4 py-2 rounded-md mx-auto">
+                            Login
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -11,15 +11,16 @@ export default function SignupPage() {
     const [passwordConfirmation, setPasswordConfirmation] = useState('');
     const [username , setUsername] = useState('');
     const router = useRouter();
-    const { data: session } = useSession();
-    const { showToastPromise } = useToastContext();
+    const { data: session, status } = useSession();
+    const { showToast, showToastPromise } = useToastContext();
 
     // check if user is already signed in
     useEffect(() => {
-        if (session) {
+        if (status === 'authenticated') {
+            showToast('You are already signed in', {}, 'error');
             router.push('/');
         }
-    }, [session, router]);
+    }, [status]);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -50,63 +51,68 @@ export default function SignupPage() {
         });
     };
 
-    return (
-        <div className="w-96 h-full mx-auto flex flex-col align-middle">
-            <div className="my-auto px-4 py-5 bg-neutral-800 text-white rounded-3xl">
-                <h1 className="text-center font-bold text-2xl mb-3">Create an Account</h1>
-                <form onSubmit={handleSubmit} className="flex flex-col justify-center">
-                    <div className="mb-3">
-                        <label htmlFor="username-input" className="ml-0.5">Username:</label>
-                        <input
-                            id="username-input"
-                            type="text"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={username}
-                            placeholder="John Doe"
-                            onChange={(e) => setUsername(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="email-input" className="ml-0.5">Email:</label>
-                        <input
-                            id="email-input"
-                            type="email"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={email}
-                            placeholder="hello@example.com"
-                            onChange={(e) => setEmail(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password-input" className="ml-0.5">Password:</label>
-                        <input
-                            id="password-input"
-                            type="password"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={password}
-                            onChange={(e) => setPassword(e.target.value)}
-                            required
-                        />
-                    </div>
-                    <div className="mb-3">
-                        <label htmlFor="password-confirmation-input" className="ml-0.5">Password Confirmation:</label>
-                        <input
-                            id="password-confirmation-input"
-                            type="password"
-                            className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
-                            value={passwordConfirmation}
-                            onChange={(e) => setPasswordConfirmation(e.target.value)}
-                            required
-                        />
-                    </div>
+    if (status === 'loading' || status === 'authenticated') {
+        return <></>
+    } else {
+        return (
+            <div className="w-96 h-full mx-auto flex flex-col align-middle">
+                <div className="my-auto px-4 py-5 bg-neutral-800 text-white rounded-3xl">
+                    <h1 className="text-center font-bold text-2xl mb-3">Create an Account</h1>
+                    <form onSubmit={handleSubmit} className="flex flex-col justify-center">
+                        <div className="mb-3">
+                            <label htmlFor="username-input" className="ml-0.5">Username:</label>
+                            <input
+                                id="username-input"
+                                type="text"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={username}
+                                placeholder="John Doe"
+                                onChange={(e) => setUsername(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="email-input" className="ml-0.5">Email:</label>
+                            <input
+                                id="email-input"
+                                type="email"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={email}
+                                placeholder="hello@example.com"
+                                onChange={(e) => setEmail(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password-input" className="ml-0.5">Password:</label>
+                            <input
+                                id="password-input"
+                                type="password"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                                required
+                            />
+                        </div>
+                        <div className="mb-3">
+                            <label htmlFor="password-confirmation-input" className="ml-0.5">Password
+                                Confirmation:</label>
+                            <input
+                                id="password-confirmation-input"
+                                type="password"
+                                className="border border-neutral-500 bg-transparent px-4 py-2 rounded-xl block w-full"
+                                value={passwordConfirmation}
+                                onChange={(e) => setPasswordConfirmation(e.target.value)}
+                                required
+                            />
+                        </div>
 
-                    <button type="submit" className="bg-blue-700 px-4 py-2 rounded-md mx-auto">
-                        Sign Up
-                    </button>
-                </form>
+                        <button type="submit" className="bg-blue-700 px-4 py-2 rounded-md mx-auto">
+                            Sign Up
+                        </button>
+                    </form>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
