e2c126ad7f426bf2ac44f7e6c066a50a8d68e7be
Charley Liu <charleyliu716@gmail.com>
2024-07-08 15:15:03 -0400

    add loading state when fetching client side session in header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -5,11 +5,11 @@ import Link from "next/link"
 import "./Header.css"
 import {LoginButton, LogoutButton, SignupButton} from "@/components/auth/auth";
 import {useSession} from "next-auth/react";
-import {Button, Divider} from "@chakra-ui/react";
+import {Button, Divider, Skeleton, Stack} from "@chakra-ui/react";
 import {useRouter} from "next/navigation";
 
 export default function Header() {
-    const { data: session } = useSession();
+    const { data: session, status } = useSession();
     const router = useRouter();
 
     const handleUserClick = () => {
@@ -38,7 +38,17 @@ export default function Header() {
                 </Link>
 
                 {/* User Profile Actions */}
-                { (session && session.user !== undefined) ? (
+                { status === 'loading' && (
+                    <div className="w-1/2 h-full flex items-center justify-end gap-x-2.5">
+                        <Stack className="w-36" gap={1.5}>
+                            <Skeleton className="!w-full !h-1.5" />
+                            <Skeleton className="!w-full !h-1.5" />
+                            <Skeleton className="!w-full !h-1.5" />
+                        </Stack>
+                    </div>
+                )}
+
+                { (status === 'authenticated' && session && session.user) && (
                     <div className="w-1/2 h-full flex items-center justify-end gap-x-2.5">
                         <Button
                             color="whiteAlpha.800"
@@ -53,7 +63,9 @@ export default function Header() {
                         <Divider orientation="vertical" className="!h-1/3" />
                         <LogoutButton />
                     </div>
-                ) : (
+                )}
+
+                { (status === 'unauthenticated') && (
                     <div className="w-1/2 h-full flex items-center justify-end gap-x-2.5">
                         <LoginButton/>
                         <Divider orientation="vertical" className="!h-1/3" />
