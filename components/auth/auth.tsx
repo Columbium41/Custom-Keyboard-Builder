e2f6e5f1577be920c7896f5f904c08028cc5b5ff
Charley Liu <charleyliu716@gmail.com>
2024-07-20 16:49:23 -0400

    add logout confirmation modal

--- components/auth/auth.tsx
+++ components/auth/auth.tsx
@@ -1,8 +1,18 @@
 'use client'
 
 import { signIn, signOut } from "next-auth/react";
-import {Button} from "@chakra-ui/react";
+import {
+    AlertDialog,
+    AlertDialogBody,
+    AlertDialogContent, AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogOverlay,
+    Button
+} from "@chakra-ui/react";
 import {useRouter} from "next/navigation";
+import {useDisclosure} from "@chakra-ui/hooks";
+import {useRef} from "react";
+import {FocusableElement} from "@chakra-ui/utils";
 
 export const LoginButton = () => {
     return (
@@ -17,16 +27,54 @@ export const LoginButton = () => {
     )
 }
 
+// logout alert dialog
 export const LogoutButton = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure()
+    const cancelRef = useRef()
+
     return (
-        <Button
-            color="inherit"
-            _hover={{
-                color: "orange.400",
-            }}
-            variant="link"
-            onClick={() => signOut()}
-        >Logout</Button>
+        <>
+            <Button
+                color="inherit"
+                _hover={{
+                    color: "orange.400",
+                }}
+                variant="link"
+                onClick={onOpen}
+            >Logout</Button>
+
+            <AlertDialog
+                isOpen={isOpen}
+                // @ts-ignore
+                leastDestructiveRef={cancelRef}
+                onClose={onClose}
+            >
+                <AlertDialogOverlay>
+                    <AlertDialogContent>
+                        <AlertDialogHeader fontSize='lg' fontWeight='bold' borderTopRadius='md'>
+                            Logout
+                        </AlertDialogHeader>
+
+                        <AlertDialogBody>
+                            Are you sure you want to logout?
+                        </AlertDialogBody>
+
+                        <AlertDialogFooter>
+                            <Button
+                                // @ts-ignore
+                                ref={cancelRef}
+                                onClick={onClose}
+                            >
+                                Cancel
+                            </Button>
+                            <Button colorScheme='red' onClick={() => signOut()} ml={3}>
+                                Logout
+                            </Button>
+                        </AlertDialogFooter>
+                    </AlertDialogContent>
+                </AlertDialogOverlay>
+            </AlertDialog>
+        </>
     )
 }
 
